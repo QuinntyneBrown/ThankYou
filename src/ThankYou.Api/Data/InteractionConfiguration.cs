00177cfead1b0c157a56ecc8b81da74f830e917e
QuinntyneBrown <quinntynebrown@gmail.com>
2021-06-18 00:24:02 -0400

    feat: entity configs

---
+++ src/ThankYou.Api/Data/InteractionConfiguration.cs
@@ -0,0 +1,15 @@
+using ThankYou.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ThankYou.Api.Data
+{
+    public class InteractionConfiguration: IEntityTypeConfiguration<Interaction>
+    {
+        public void Configure(EntityTypeBuilder<Interaction> builder)
+        {
+            
+        }
+        
+    }
+}
---
+++ src/ThankYou.Api/Data/ParticipantConfiguration.cs
@@ -0,0 +1,15 @@
+using ThankYou.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ThankYou.Api.Data
+{
+    public class ParticipantConfiguration: IEntityTypeConfiguration<Participant>
+    {
+        public void Configure(EntityTypeBuilder<Participant> builder)
+        {
+            
+        }
+        
+    }
+}
